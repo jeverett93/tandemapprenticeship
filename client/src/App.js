79b65ed8a2d883283a819f5af74bd806cdb40e85
Josh Everett <josheverett93@gmail.com>
2020-10-27 18:37:49 -0500

    added JSON object with quiz data and created file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import FriendCard from "./components/FriendCard";
+// import Wrapper from "./components/Wrapper";
+// import Title from "./components/Title";
+import quiz_data from "./quiz_data.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    quiz_data
+  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const quiz_data = this.state.quiz_data.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.quiz_data.map(quiz => (
+          <FriendCard
+            // removeFriend={this.removeFriend}
+            question={quiz.question}
+            // key={friend.id}
+            choices={quiz.choices}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
+// will need a page like this for each question. Probably should add id to each object
+// or make quiz num variable that starts at 0 and iterates
 export default App;
